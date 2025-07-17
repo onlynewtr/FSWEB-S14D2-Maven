5af9ae2a92bdc2db18ebf8953d4465cf10e7a0cb
barisozcan93 <barisozcn93@gmail.com>
2025-07-17 23:34:19 +0300

    solution added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,33 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.model.enums.LampType;
+import org.example.model.enums.PaintColor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Lamp lamp = new Lamp(LampType.NEON, true, 100);
+        lamp.turnOn();
+
+        Ceiling ceiling = new Ceiling(3, PaintColor.RED);
+        ceiling.create();
+
+        Bed bed = new Bed("double", 2, 2, 2, 2);
+        System.out.println(bed.toString());
+
+        Wall northWall = new Wall("North");
+        Wall eastWall = new Wall("East");
+        Wall westWall = new Wall("West");
+        Wall southWall = new Wall("South");
+
+        Wall[] walls = {northWall, eastWall, westWall, southWall};
+
+        Bedroom bedRoom = new Bedroom(northWall, eastWall, southWall,
+                westWall, ceiling, lamp,
+                new Carpet(100, 200, PaintColor.WHITE), "bedroom1", bed,
+                new Wardrobe(1, 2, 12));
+        System.out.println(bedRoom);
+        bedRoom.createBedroom();
+
     }
 }
